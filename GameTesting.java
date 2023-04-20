342aa33a47ebcd783a096bc8e379bd1f65bf105f
Pally35 <130520159+Pally35@users.noreply.github.com>
2023-04-20 10:14:15 -0400

    Add files via upload
    w/ basic customer

--- GameTesting.java
+++ GameTesting.java
@@ -18,21 +18,56 @@ https://www.cuemath.com/geometry/radians-to-degrees/
 https://www.educba.com/repaint-in-java/
  */
 class Cat{
+    String desiredOrder;
+    int impatienceFactor; //This could be the variable that measures how quickly the customer wants their order,
+    //the score that the player gets could be deterimend by comparing this value with the time it took to complete the order
+    Boolean isOrderFilled = false;
+    long startTime;
+    long endTime;
+    int xPosition;
+    int yPosition;
+    public Cat(String order, int impatienceFactor) {
+        desiredOrder = order;
+        startTime = System.nanoTime(); //tracks time when customer was created. Once order is fulfilled can compare start time
+        //with time that order was fulfilled at
+
+    }
+    public void update(GameWorld w) {
+
+        if (isOrderFilled) {
+            //calculate score
+            long timeElapsed = endTime - startTime;
+
+
+            w.currentCustomers.remove(this);//remove from list of current customers
+            //add new customer?
+        }
+    }
+
+
+
+
 
 }
 
 class GameWorld{
     int height;
     int width;
+    int score = 0; //tracks player score
+    ArrayList<Cat> currentCustomers; //list of current customers
 
     public GameWorld(int initWidth, int initHeight){
         width = initWidth;
         height = initHeight;
+        currentCustomers = new ArrayList<Cat>();
+        Cat testCat = new Cat("coffee", 1);
+        currentCustomers.add(testCat);
     }
 
     public void drawShapes(Graphics g){ //draw shape in shapes array
     }
-    public void updateShapes(double time){
+    public void updateCustomers(){
+
     }
 }
 
@@ -40,7 +75,7 @@ public class GameTesting extends JPanel implements KeyListener, MouseListener {
     public static final int WIDTH = 900;
     public static final int HEIGHT = 400;
     public static final int FPS = 60;
-    World world;
+    GameWorld world;
     String background;
     ArrayList<String> addedIngredients;
     class Runner implements Runnable{
@@ -58,7 +93,7 @@ public class GameTesting extends JPanel implements KeyListener, MouseListener {
         addMouseListener(this);
         addedIngredients=new ArrayList<String>();
         background = "StartScreen";
-        world = new World(WIDTH, HEIGHT);
+        world = new GameWorld(WIDTH, HEIGHT);
         addKeyListener(this);
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         Thread mainThread = new Thread(new GameTesting.Runner());
@@ -93,6 +128,22 @@ public class GameTesting extends JPanel implements KeyListener, MouseListener {
                 if(x < 880 && x > 690 && y<130 && y > 65){ //if you click the button
                     background = "KitchenCounter";
                     break;
+                }
+                for (Cat customer : world.currentCustomers) { //basic form of code, need to figure how to check exactly if a customer was clicked
+                    if (x < customer.xPosition && x > customer.xPosition - 10 && y < customer.yPosition && y > customer.yPosition - 10) {
+                        String preparedOrder = "";
+                        for (String ingredient : addedIngredients) { //takes all of the added ingredients and converts to one string
+                            preparedOrder += ingredient + " ";
+                        }
+                        if (customer.desiredOrder == preparedOrder) { // if desired order is equal to prepared order
+                            for (String ingredient : addedIngredients) {
+                                addedIngredients.remove(ingredient); // removes ingredients from added ingredients
+                            }
+                            customer.endTime = System.nanoTime(); // will be used to calcuate elapsed time
+                            customer.isOrderFilled = true;
+                        }
+                        break;
+                    }
                 }
         }
     }
